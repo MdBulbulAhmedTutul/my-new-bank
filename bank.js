c447e8ecaac703eeac38a77d3a19af724effb3c4
MdBulbulAhmedTutul <tutulwebdevoloper@gmail.com>
2023-08-17 08:43:17 -0700

    withdraw balance added there

--- bank.js
+++ bank.js
@@ -1,4 +1,4 @@
-// Deposit and withdraw Js setting
+// Deposit btn Js setting
 document.getElementById('deposit-btn').addEventListener('click', function(){
 
     // Step 1: get the deposit value
@@ -21,7 +21,41 @@ document.getElementById('deposit-btn').addEventListener('click', function(){
     const currentBalanceString = currentBalance.innerText;
     const currentBalanceNumber = parseFloat(currentBalanceString);
 
-    // Step 5: Set The Account Balance
+    // Step 5: Set The Account Balance before the deposit
     const newCurrentBalace = currentBalanceNumber + depositAmmountNumber;
     currentBalance.innerText = newCurrentBalace;
+});
+
+
+// Withdraw Btn js setting
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+
+    // Step 1: get withdraw Value
+    const withdrawAmount = document.getElementById('withdrw-amount');
+    const withdrawAmountString = withdrawAmount.value;
+    const withdrawAmountNumber = parseFloat(withdrawAmountString);
+    withdrawAmount.value = '';
+    
+    // Step 2: get the current witdraw Money
+    const witdrawMoney = document.getElementById('witdraw-money');
+    const witdrawMoneyString = witdrawMoney.innerText;
+    const witdrawMoneyNumber = parseFloat(witdrawMoneyString);
+    
+    // Step 3: set the withdraw money
+    const newWithdrawMoney = witdrawMoneyNumber + withdrawAmountNumber;
+    witdrawMoney.innerText = newWithdrawMoney;
+
+    // Step 4: get the current balance
+    const currentBalance = document.getElementById('curent-balance');
+    const currentBalanceString = currentBalance.innerText;
+    const currentBalanceNumber = parseFloat(currentBalanceString);
+
+    // Step 5: Set The Account Balance brfore withdraw
+    const updateCurentBalance = currentBalanceNumber - withdrawAmountNumber;
+    currentBalance.innerText = updateCurentBalance;
+
+    // Step 6: condition current balance and withdraw Balance
+    if(withdrawAmountNumber > currentBalanceNumber){
+        alert('Check Your Current balance');
+    }
 });
